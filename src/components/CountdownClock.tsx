1e5595c318791bfb096144efe49db536b7a7c596
Carl Licata <cjlicata@gmail.com>
2022-01-10 19:36:44 -0500

    Refactors CountdownClock for readability

--- src/components/CountdownClock.tsx
+++ src/components/CountdownClock.tsx
@@ -15,19 +15,18 @@ const getIntervalString = ({
 }): string => {
   const totalSeconds = Math.max(
     0,
-    differenceInSeconds(to, from, {
-      roundingMethod: 'floor',
-    })
+    differenceInSeconds(to, from, { roundingMethod: 'floor' })
   );
 
   const hours = Math.floor(totalSeconds / 60 / 60);
   const minutes = Math.floor((totalSeconds - hours * 60 * 60) / 60);
   const seconds = totalSeconds - hours * 60 * 60 - minutes * 60;
 
-  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(
-    2,
-    '0'
-  )}:${String(seconds).padStart(2, '0')}`;
+  return [
+    String(hours).padStart(2, '0'),
+    String(minutes).padStart(2, '0'),
+    String(seconds).padStart(2, '0'),
+  ].join(':');
 };
 
 const CountdownClock = ({ endAt }: Props) => {
