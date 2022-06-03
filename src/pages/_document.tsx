dbfbddc753eae0d302a0143db9fd0bd0d68e4706
Carl Licata <cjlicata@gmail.com>
2022-06-03 19:45:11 -0400

    Sets an html lang

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html lang="en-US">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
