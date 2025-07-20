09730573428ba32e746ad07ae8cb1d6b9b32653e
Carl Licata <cjlicata@gmail.com>
2025-07-20 00:25:26 -0400

    fix: metadata improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import { Analytics } from '@vercel/analytics/next';
+import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 
 import '../theme/global.css';
+import { ORIGIN } from '@app/config/public';
+
+const title = 'SQWORDLE';
+
+export const metadata = {
+  metadataBase: new URL(ORIGIN),
+
+  description: 'A Pokémon-themed take on Wordle',
+  title,
+} satisfies Metadata;
 
 const Layout = ({ children }: { children: ReactNode }) => (
   <html lang="en-US">
